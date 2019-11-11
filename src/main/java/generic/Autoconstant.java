287503b93ba52dbed5d7be3a4aadbb18473aaf2c
Admin <Admin@DESKTOP-R8AQTSO>
2019-11-11 11:08:38 +0530

    commit

--- src/main/java/generic/Autoconstant.java
+++ src/main/java/generic/Autoconstant.java
@@ -1,5 +1,15 @@
 package generic;
 
-public interface Autoconstant {
+public interface Autoconstant 
+{
+	String chrome_key = "webdriver.chrome.driver";
+	String chrome_value ="./src/main/resources/Drivers/chromedriver.exe";
+	String firefox_key = "webdriver.gecko.driver";
+	String firefox_value ="./src/main/resources/Drivers/geckodriver.exe";
+	String ie_key = "webdriver.ie.driver";
+	String ie_value ="./src/main/resources/Drivers/IEDriverServer.exe";
 
+	String file_path="./src/test/resources/Data/input.xlsx";
 }
+
+
--- src/main/java/generic/BaseTest.java
+++ src/main/java/generic/BaseTest.java
@@ -1,5 +1,25 @@
 package generic;
 
-public class BaseTest {
+import java.util.concurrent.TimeUnit;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class BaseTest implements Autoconstant
+{
+	public WebDriver driver;
+	public void preCondition()
+	{
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+		driver.get("http://192.168.233.128/dolibarr-3.3.1/htdocs/");
+	}
+	
+	public void postCondition()
+	{
+		driver.close();
+		
+	}
 }
